f87b622e92c2ed6324bfeb6c1b91c2351e69ebd3
myronn8330 <91744974+myronn8330@users.noreply.github.com>
2021-10-01 18:13:19 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let ownerName = "Myron Ng"
+let userName = "myronn8300";
+
+document.querySelectorAll(".owner-name").forEach((e) => {
+    e.innerHTML = ownerName;
+});
+
+document.querySelector("#github").href = "http://github.com/" + userName;
+document.querySelector("#fork").href = "http://github.com/" + userName + "/" + userName + ".github.io";
